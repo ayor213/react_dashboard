b7d0419c43162b4c3152ffd351390f57c7d91535
ayor <ayorfolorunso@gmail.com>
2024-03-25 13:48:55 +0000

    team

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -5,7 +5,7 @@ import Sidebar from "./scenes/global/Sidebar.jsx";
 import Dashboard from "./scenes/dashboard";
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-// import Team from "./scenes/team";
+import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
@@ -31,35 +31,11 @@ function App() {
         <CssBaseline />
         <div className="app">
           <Sidebar isSidebar={isSidebar}/>
-            {/* <Menu>
-              <MenuItem className="menu1">
-                <h2>QUICKPAY</h2>
-              </MenuItem>
-              <MenuItem> Dashboard </MenuItem>
-              <MenuItem> Invoices </MenuItem>
-              <SubMenu label="Charts">
-                <MenuItem> Timeline Chart </MenuItem>
-                <MenuItem> Bubble Chart </MenuItem>
-              </SubMenu>
-              <SubMenu label="Wallets">
-                <MenuItem>Current Wallet</MenuItem>
-                <MenuItem>Savings Wallet</MenuItem>
-              </SubMenu>
-              <MenuItem> Transactions </MenuItem>
-              <SubMenu label="Settings">
-                <MenuItem> Account </MenuItem>
-                <MenuItem> Privacy </MenuItem>
-                <MenuItem> Notifications </MenuItem>
-              </SubMenu>
-              <MenuItem> Logout </MenuItem>
-            </Menu> */}
-          
-         {/*  <Sidebar isSidebar={isSidebar} /> */}
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} /> */}
+              <Route path="/team" element={<Team />} />
               {/* <Route path="/contacts" element={<Contacts />} /> */}
               {/* <Route path="/invoices" element={<Invoices />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
@@ -68,8 +44,8 @@ function App() {
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/calendar" element={<Calendar />} /> */}
-              {/* <Route path="/geography" element={<Waterfall />} /> */}
-              {/* <Route path="/boxPlot" element={<Waterfall />} /> */}
+              {/* <Route path="/geography" element={<Map />} /> */}
+              {/* <Route path="/boxPlot" element={<Boxplot />} /> */}
               {/* <Route path="/waterfall" element={<Waterfall />} /> */}
             </Routes>
           </main>
